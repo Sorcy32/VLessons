3e92b8cddcaafa20dc202fd0ce060cdcf2fe997b
Sorcy32 <Sorcerer2005@yandex.ru>
2019-06-02 01:10:47 +0300

    date_time_solved

--- date_time.py
+++ date_time.py
@@ -1,17 +1,22 @@
-    
 """
 Домашнее задание №2
 Дата и время
 * Напечатайте в консоль даты: вчера, сегодня, месяц назад
 * Превратите строку "01/01/17 12:10:03.234567" в объект datetime
 """
+from datetime import datetime, timedelta
+
 
 def print_days():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    dt=datetime.now()
+    print('Вчера      : ', datetime.strftime(dt+timedelta(days=-1), "%d.%m.%Y"))
+    print('Сегодня    : ', datetime.strftime(dt, "%d.%m.%Y"))
+    print('Месяц назад: ', datetime.strftime(dt.replace(month=(dt.month-1)), "%d.%m.%Y"))
+
     
     
 def str_2_datetime(string):
@@ -19,7 +24,8 @@ def str_2_datetime(string):
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    dt=datetime.strptime(string, "%d/%m/%y %X.%f")
+    return dt
 
 if __name__ == "__main__":
     print_days()
